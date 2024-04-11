109faa7dc5ec4a2a891100641206bb53897d2e7b
Ya-Chi Chu <ycchu97@stanford.edu>
2024-04-11 15:29:24 -0700

    adds portfolio analysis

---
+++ notebooks/Portfolio_analysis/get_demonstrate_results.jl
@@ -0,0 +1,22 @@
+####################################################################################################
+## Copy the latest 6 results to notebooks/Portfolio_analysis/demonstrate_results folder
+####################################################################################################
+using DrWatson
+@quickactivate "."
+
+# Define the source and destination paths
+source_path = scriptsdir("Portfolio", "results", "risk_model", "IPPMM")
+destination_path = projectdir("notebooks", "Portfolio_analysis", "demonstrate_results")
+
+# Get a list of all CSV files in the source path
+csv_files = filter(x -> x[end-3:end] == ".csv", readdir(source_path))
+
+# Get the latest 6 files in the source path
+latest_files = sort(collect(readdir(source_path)), rev=true)[1:6]
+
+# Copy each of the latest 6 CSV files to the destination path
+for file in latest_files
+    source_file = joinpath(source_path, file)
+    destination_file = joinpath(destination_path, file)
+    cp(source_file, destination_file)
+end
